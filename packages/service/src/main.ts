bd294aa413a45fdcc948afb01e9d16122c9f4c3f
Christopher Jaeger <christopher@mogler.dev>
2023-09-02 09:03:29 +0200

    service: starting with main

--- packages/service/src/main.ts
+++ packages/service/src/main.ts
@@ -1,3 +1,18 @@
-import "./xx/hello";
+import express from "express";
+import { nanoid } from "nanoid";
 
-console.log("Hello");
+
+const app = express();
+const port = 3000;
+
+app.get("/_/:path", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/:code?", (req, res) => {
+  res.send(nanoid(10));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
